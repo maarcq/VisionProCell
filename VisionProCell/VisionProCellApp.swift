5800ea35df13b75e206b171636f48d98f593df4b
maarcq <marcellerq27@gmail.com>
2025-06-03 16:00:03 -0300

    Initial Commit

---
+++ VisionProCell/VisionProCellApp.swift
@@ -0,0 +1,18 @@
+//
+//  VisionProCellApp.swift
+//  VisionProCell
+//
+//  Created by Marcelle Ribeiro Queiroz on 03/06/25.
+//
+
+import SwiftUI
+
+@main
+struct VisionProCellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.volumetric)
+    }
+}
