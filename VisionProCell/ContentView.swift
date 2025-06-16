c8f67b6ed136d2f44a261c993dada5965b49aa7e
maarcq <marcellerq27@gmail.com>
2025-06-15 22:11:44 -0300

    ultimo commit

--- VisionProCell/ContentView.swift
+++ VisionProCell/ContentView.swift
@@ -20,13 +20,6 @@ struct ContentView: View {
                 // Add the initial RealityKit content
                 if let scene = try? await Entity(named: "Cell", in: realityKitContentBundle) {
                     self.scene = scene
-                    
-//                    // Percorre todos os filhos da cena para configurar animações em loop
-//                    let children = scene.children[0].children
-//                    for child in children {
-//                        setupAnimationLoop(for: child)
-//                    }
-
                     content.add(scene)
                 }
             } update: { content in
@@ -36,10 +29,7 @@ struct ContentView: View {
                     scene.transform.scale = [uniformScale, uniformScale, uniformScale]
                 }
             }
-            .gesture(TapGesture().targetedToAnyEntity().onEnded { value in
-                print(value.entity.name)
-            })
-
+            
             VStack {
                 Button {
                     enlarge.toggle()
@@ -54,18 +44,6 @@ struct ContentView: View {
             .glassBackgroundEffect()
         }
     }
-//    // Função para configurar animação em loop
-//    private func setupAnimationLoop(for entity: Entity) {
-//        // Verifica se a entidade tem animações
-//        guard let animationResource = entity.availableAnimations.first else {
-//            print("Não tem animação na entity: \(entity.name)")
-//            return
-//        }
-//        // Cria uma ação de animação em loop
-//        let loopAnimation = animationResource.repeat()
-//        
-//        entity.playAnimation(loopAnimation)
-//    }
 }
 
 #Preview(windowStyle: .volumetric) {
